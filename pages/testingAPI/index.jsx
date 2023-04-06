05ec8c95798fcfd4aa7f1d3100079a268f1dc3d8
Jamar Andrade <jandrade2018@fau.edu>
2023-04-06 17:10:00 -0400

    test: testing commit

---
+++ pages/testingAPI/index.jsx
@@ -0,0 +1,60 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
+function TestingAPI() {
+    const [movies, setMovies] = useState([])
+    
+    useEffect( () => {
+        const apiCall = async() =>{
+            const response = await axios.get(`https://api.themoviedb.org/3/trending/all/day?api_key=70184d4cf9efbb553ccc3b24b8b2f10b`)
+            const arr = []
+            const data = response.data.results
+            const imgLink = "http://image.tmdb.org/t/p/w220_and_h330_face"
+            
+            console.log(data);
+            data.forEach((el, idx) => {
+                const movie = {
+                    id: el.id || idx,
+                    name: el.name || el.original_title,
+                    overview: el.overview || '',
+                    img: `${imgLink}${el.poster_path}`,
+                    release_date: el.release_date || ''
+                }
+                arr.push(movie)
+            })
+            setMovies(arr)
+        }
+        apiCall()
+        console.log(movies)
+    }, [])
+
+    const showFeaturedMovies = async () => {
+        const response = await axios.get("/api/showFeaturedMovies");
+        const { data } = response
+        console.log(data)
+    }
+
+    return (
+        <>
+            <h1>HELLO WORLD!</h1>
+            <button type="button" onClick={showFeaturedMovies}>
+                test backed for featured movies
+            </button>
+        </>
+    );
+}
+
+// use this if we want our server to refresh this page each time our backend gets a request.
+
+// export async function getStaticProps() {
+//   const client = await MongoClient.connect(`${process.env.MONGODB_URI}`);
+//   const db = client.db();
+//   client.close();
+
+//   return {
+//     props: {},
+//     revalidate: 1,
+//   };
+// }
+
+export default TestingAPI;
