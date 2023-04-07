3cfa4c53cec889d7708c8ecdfe93a8ba42625178
ColtonRohan <crohan2020@fau.edu>
2023-04-07 13:39:32 -0400

    style: moved the button to below the movie image, added styling for the buttons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,7 @@
-import { initFirebase } from '@/firebase/firebaseApp';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth';
-import { useAuthState } from 'react-firebase-hooks/auth';
-import Movies from './movies';
-
+import { initFirebase } from '@/firebase/firebaseApp'
+import { getAuth, GoogleAuthProvider, signInWithPopup } from 'firebase/auth'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import Movies from './movies'
 
 export default function Home() {
   // firebase configuration
@@ -42,14 +41,24 @@ export default function Home() {
     return (
       <div className="p-10 mb-20">
         {/*  <p>Welcome {user.displayName}</p> */}
-        <div className="text-2xl">Movies</div>
-        <button type="button" onClick={signOut}>
-          Sign OUT
-        </button>
-        <button type="button" onClick={callApi}>
-          Get token
-        </button>
-        <Movies/>
+        <div className="grid justify-center py-6 text-center">
+          <div className="text-2xl">List of Popular Movies For the Day</div>
+          <button
+            className="bg-[rgb(31,41,55)] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+            type="button"
+            onClick={signOut}
+          >
+            Sign OUT
+          </button>
+          {/* <button
+            className="bg-[rgb(31,41,55)] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+            type="button"
+            onClick={callApi}
+          >
+            Get token
+          </button> */}
+        </div>
+        <Movies />
       </div>
     )
   }
@@ -57,7 +66,7 @@ export default function Home() {
   return (
     <div className="bg-white flex justify-center">
       <button
-        className=" bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
+        className=" bg-[rgb(31,41,55)] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full"
         type="button"
         onClick={signIn}
       >
--- pages/movies.js
+++ pages/movies.js
@@ -1,68 +1,75 @@
-import axios from 'axios';
-import Image from 'next/image';
-import { useEffect, useState } from 'react';
-
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 export default function Movies() {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState([])
 
   useEffect(() => {
     const getMovies = async () => {
       try {
-        const movieApiResponse = await axios.get('/api');
-        const movieData = movieApiResponse.data;
-        setMovies(movieData);
+        const movieApiResponse = await axios.get('/api')
+        const movieData = movieApiResponse.data
+        setMovies(movieData)
       } catch (error) {
-        console.error(error);
+        console.error(error)
       }
-    };
-    getMovies();
-  }, []);
+    }
+    getMovies()
+  }, [])
   console.log(movies)
 
   const addMovie = async (movie) => {
     try {
-      console.log('clicked...');
+      console.log('clicked...')
       const response = await axios.post('/api', movie, {
         headers: {
           'Content-Type': 'application/json',
         },
-      });
+      })
 
-      console.log(response);
+      console.log(response)
     } catch (error) {
-      console.error(error);
+      console.error(error)
     }
-  };
+  }
 
   return (
-    <ul className="px-3 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+    <ul className="px-3 grid grid-cols-2 gap-x-4 gap-y-8 sm:grid-cols-3 sm:gap-x-6 lg:grid-cols-7 xl:gap-x-8">
       {movies.map((movie) => (
-        <>
-        <li key={movie.id} className="relative">
-          <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
-            <Image
-              src={movie.image}
-              alt=""
-              className="pointer-events-none object-cover group-hover:opacity-75"
-            />
-            <button type="button" className="absolute inset-0 focus:outline-none">
-              <span className="sr-only">View details for {movie.title}</span>
-            </button>
-          </div>
-          <p className="pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
-            {movie.title}
-          </p>
-          <p className="pointer-events-none block text-sm font-medium text-gray-600">
-            {movie.release_date}
-          </p>
-          <p className="pointer-events-none block text-sm font-medium text-gray-400">
-            {movie.popularity}
-          </p>
-        </li>
-        <button type="button" onClick={() => addMovie(movie)}>Add Movie</button>
-      </>
+        <div>
+          <li key={movie.id} className="relative">
+            <div className="group aspect-h-7 aspect-w-10 block w-full overflow-hidden rounded-lg bg-gray-100 focus-within:ring-2 focus-within:ring-indigo-500 focus-within:ring-offset-2 focus-within:ring-offset-gray-100">
+              <img
+                src={movie.image}
+                alt=""
+                className="pointer-events-none object-cover group-hover:opacity-75"
+              />
+              <button
+                type="button"
+                className="absolute inset-0 focus:outline-none"
+              >
+                <span className="sr-only">View details for {movie.title}</span>
+              </button>
+            </div>
+            <p className="text-center pointer-events-none mt-2 block truncate text-sm font-medium text-gray-900">
+              {movie.title}
+            </p>
+            <p className="text-center pointer-events-none block text-sm font-medium text-gray-600">
+              {movie.release_date}
+            </p>
+            <p className="text-center pointer-events-none block text-sm font-medium text-gray-400">
+              {movie.popularity}
+            </p>
+          </li>
+          <button
+            className="bg-[rgb(31,41,55)] hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full content-center"
+            type="button"
+            onClick={() => addMovie(movie)}
+          >
+            Add Favorites
+          </button>
+        </div>
       ))}
     </ul>
-  );
+  )
 }
